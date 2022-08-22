7a645a93418d1c605279372c5cf3e03f41638972
JAYASHRINIDHI <108124890+JAYASHRINIDHI@users.noreply.github.com>
2022-08-22 12:31:58 +0530

    Add files via upload

---
+++ DOCUMENT2.R
@@ -0,0 +1,20 @@
+data2 <- read.csv("https://databank.worldbank.org/data/download/GDP.csv",header=T) 
+View(data2)
+
+data3 <- read.csv("https://example-files.online-convert.com/document/txt/example.txt",header=T) 
+View(data3)
+
+data4 <- read.csv("D:/jayashrinidhi/b11.csv")
+View(data4)
+data4 <- read.csv("D:/jayashrinidhi/b11.csv")
+print(data4)
+
+data5 <- read.delim("D:/jayashrinidhi/a11.txt")
+View(data5)
+data5 <- read.delim("D:/jayashrinidhi/a11.txt")
+print(data5)
+
+library(readxl)
+data6 <- read_excel("D:/jayashrinidhi/C11.xlsx")
+View(data6)
+print(data6)
